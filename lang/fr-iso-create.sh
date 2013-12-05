73ae1ed99d1514a71aec783cf12e3b06632adb26
Kimpe Andy <andykimpe@gmail.com>
2013-12-05 15:28:25 +0100

    Update fr-iso-create.sh

--- lang/fr-iso-create.sh
+++ lang/fr-iso-create.sh
@@ -2,3 +2,5 @@
 CENTOS="Pour créer une iso Centos, tapez c"
 UBUNTU="Pour créer une iso Ubuntu, tapez u"
 QUIT="Pour quitter : CTRL-C"
+64="pour créer une iso 64 bit tapez 64"
+32="pour créer une iso 32 bit tapez 32"
