6987fac6d0f4f69d2e51fad67fecbfe9e4559b94
Kimpe Andy <andykimpe@gmail.com>
2013-12-05 16:10:10 +0100

    Update en-iso-create.sh

--- lang/en-iso-create.sh
+++ lang/en-iso-create.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-CENTOS="For créate iso Centos, type c"
-UBUNTU="For créate iso Ubuntu, type u"
+CENTOS="For create iso Centos, type c"
+UBUNTU="For create iso Ubuntu, type u"
 QUIT="For Exit : CTRL-C"
-BITS64="For créate iso 64 bit type 64"
-BITS32="For créate iso 32 bit type 32"
+BITS64="For create iso 64 bit type 64"
+BITS32="For create iso 32 bit type 32"
 NOTDOWNLOAD="iso and already on the hard drive no download required"
 DOWNLOAD="Download iso file"
 CREATEMOUNTDIR="Creating the mount directory"
