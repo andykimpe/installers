f2d0f09c76aac1f59c6fe7e2e06d037684854867
Kimpe Andy <andykimpe@gmail.com>
2013-12-05 15:32:08 +0100

    Update en-iso-create.sh

--- lang/en-iso-create.sh
+++ lang/en-iso-create.sh
@@ -2,5 +2,5 @@
 CENTOS="For créate iso Centos, type c"
 UBUNTU="For créate iso Ubuntu, type u"
 QUIT="For Exit : CTRL-C"
-64="For créate iso 64 bit type 64"
-32="For créate iso 32 bit type 32"
+BITS64="For créate iso 64 bit type 64"
+BITS32="For créate iso 32 bit type 32"
