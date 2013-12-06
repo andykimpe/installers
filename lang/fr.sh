6efe658d5110c04b72b088a9b73f976b9e055056
Kimpe Andy <andykimpe@gmail.com>
2013-12-06 20:52:12 +0100

    Update fr.sh

--- lang/fr.sh
+++ lang/fr.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 installroot="installation échoué! Pour installer, vous devez être connecté en tant que 'root', s'il vous plaît essayer à nouveau"
 upgraderoot="La mise à niveau a échoué! Pour mettre à niveau, vous devez être connecté en tant que 'root', s'il vous plaît essayer à nouveau"
+panel='Vous semblez avoir un panneau de contrôle est déjà installé sur votre serveur; Ce programme d installation\n'
+panel=$panel'est conçu pour installer et configurer ZPanel sur une installation du système d exploitation propre seulement!\n\n'
+panel=$panel'SVP réinstaller votre système d exploitation avant d installer en utilisant ce script.'
