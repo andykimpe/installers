e53b6132881f84eee534e50116ce1e2594cbdd68
Kimpe Andy <andykimpe@gmail.com>
2013-12-06 20:56:15 +0100

    Update en.sh

--- lang/en.sh
+++ lang/en.sh
@@ -4,3 +4,4 @@ upgraderoot="Upgrade failed! To upgrade you must be logged in as 'root', please 
 panel='You appear to have a control panel already installed on your server; This installer\n'
 panel=$panel'is designed to install and configure ZPanel on a clean OS installation only!\n\n'
 panel=$panel'Please re-install your OS before attempting to install using this script.'
+installsyserror="Sorry, this installer only supports the installation of ZPanel on"
