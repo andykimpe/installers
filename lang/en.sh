4265566a8188c33b89f9a33c26d7d95a8d37386b
Kimpe Andy <andykimpe@gmail.com>
2013-12-06 20:49:35 +0100

    Update en.sh

--- lang/en.sh
+++ lang/en.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 installroot="Installed failed! To install you must be logged in as 'root', please try again"
 upgraderoot="Upgrade failed! To upgrade you must be logged in as 'root', please try again"
+panel='You appear to have a control panel already installed on your server; This installer\n'
+panel=$panel'is designed to install and configure ZPanel on a clean OS installation only!\n\n'
+panel=$panel'Please re-install your OS before attempting to install using this script.'
