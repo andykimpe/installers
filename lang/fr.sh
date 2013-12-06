66f4a5c0b0538b90528fab48af44d24f5c78ff34
Kimpe Andy <andykimpe@gmail.com>
2013-12-06 20:44:04 +0100

    Update fr.sh

--- lang/fr.sh
+++ lang/fr.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 installroot="installation échoué! Pour installer, vous devez être connecté en tant que 'root', s'il vous plaît essayer à nouveau"
+upgraderoot="La mise à niveau a échoué! Pour mettre à niveau, vous devez être connecté en tant que 'root', s'il vous plaît essayer à nouveau"
