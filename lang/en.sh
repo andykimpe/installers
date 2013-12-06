b41f25ac92e7f511ec64e1dff386420d70b4153f
Kimpe Andy <andykimpe@gmail.com>
2013-12-06 20:42:35 +0100

    Update en.sh

--- lang/en.sh
+++ lang/en.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 installroot="Installed failed! To install you must be logged in as 'root', please try again"
+upgraderoot="Upgrade failed! To upgrade you must be logged in as 'root', please try again"
