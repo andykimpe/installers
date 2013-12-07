6041556dd7a34e765cd200b118df843703e45c9f
Kimpe Andy <andykimpe@gmail.com>
2013-12-07 02:27:01 +0100

    Update fr.sh

--- lang/fr.sh
+++ lang/fr.sh
@@ -5,6 +5,7 @@ OS="CentOs"
 else
  OS=$(cat /etc/lsb-release | grep DISTRIB_ID | sed 's/^.*=//')
   VER=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | sed 's/^.*=//')
+ fi
 osdetect="Detection"
 installroot="installation échoué! Pour installer, vous devez être connecté en tant que 'root', s'il vous plaît essayer à nouveau"
 upgraderoot="La mise à niveau a échoué! Pour mettre à niveau, vous devez être connecté en tant que 'root', s'il vous plaît essayer à nouveau"
