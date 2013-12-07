e80977827eb2708bf13b8d293fd06e11ca8178a1
Kimpe Andy <andykimpe@gmail.com>
2013-12-07 02:20:21 +0100

    Update fr.sh

--- lang/fr.sh
+++ lang/fr.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+OS="$(cat /root/os)"
+OS="$(cat /root/ver)"
 osdetect="Detection"
 installroot="installation échoué! Pour installer, vous devez être connecté en tant que 'root', s'il vous plaît essayer à nouveau"
 upgraderoot="La mise à niveau a échoué! Pour mettre à niveau, vous devez être connecté en tant que 'root', s'il vous plaît essayer à nouveau"
