5c44c79e1f693f72e4da051a5e7800ff1b341a63
Kimpe Andy <andykimpe@gmail.com>
2013-12-07 02:22:49 +0100

    Update 10_1_1.sh

--- install/CentOS-6_4/10_1_1.sh
+++ install/CentOS-6_4/10_1_1.sh
@@ -56,8 +56,6 @@ BITS=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
 if [ -f /etc/centos-release ]; then
   OS="CentOs"
   VER=$(cat /etc/centos-release | sed 's/^.*release //;s/ (Fin.*$//')
-echo CentOS > /root/os
-echo $VER > /root/ver
 else
   OS=$(uname -s)
   VER=$(uname -r)
