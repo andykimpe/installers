76a7ab1bd26496b564708f920e6cce56aa22cd7e
Kimpe Andy <andykimpe@gmail.com>
2013-12-31 07:37:06 +0100

    Update 10_1_1.sh

--- install/beta/Debian-7/10_1_1.sh
+++ install/beta/Debian-7/10_1_1.sh
@@ -235,13 +235,10 @@ chmod +s /etc/zpanel/panel/bin/zsudo
 
 # MySQL specific installation tasks...
 service mysql start
-until mysql -u root -e ";" > /dev/null 2>&1 ; do
-read -s -p "Enter Your current root Password of mysql : " password
-sqlinstall=yes
-done
-if [ "sqlinstall" != "yes" ] ; then
 mysqladmin -u root password $password
-fi
+until mysql -u root -p$password -e ";" > /dev/null 2>&1 ; do
+read -s -p "Enter Your current root Password of mysql : " password
+done
 mysql -u root -p$password -e "DROP DATABASE test";
 mysql -u root -p$password -e "DELETE FROM mysql.user WHERE User='root' AND Host != 'localhost'";
 mysql -u root -p$password -e "DELETE FROM mysql.user WHERE User=''";
