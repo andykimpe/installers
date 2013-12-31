2c5c48ca6c923ac267d0a2b401ec8576e41f48bc
Kimpe Andy <andykimpe@gmail.com>
2013-12-31 08:31:39 +0100

    Update 10_1_1.sh

--- install/beta/Feodra-17/10_1_1.sh
+++ install/beta/Feodra-17/10_1_1.sh
@@ -118,9 +118,9 @@ echo "echo \$TZ > /etc/timezone" >> /usr/bin/tzselect
 while true; do
         tzselect
         tz=`cat /etc/timezone`
-        read -e -p "Enter the FQDN of the server (example: zpanel.yourdomain.com): " -i $fqdn fqdn
-        read -e -p "Enter the public (external) server IP: " -i $publicip publicip
-        read -e -p "Enter your Email address" email
+        read -e -p "Enter the FQDN of the server (example: zpanel.yourdomain.com): " -i $fqdn fqdn &>/dev/tty
+        read -e -p "Enter the public (external) server IP: " -i $publicip publicip &>/dev/tty
+        read -e -p "Enter your Email address" email &>/dev/tty
         read -e -p "ZPanel is now ready to install, do you wish to continue (y/n)" yn
         case $yn in
                 [Yy]* ) break;;
