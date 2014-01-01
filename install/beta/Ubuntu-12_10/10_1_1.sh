823293fb8e0b78ec0a5bb77625e5ed2649f119f4
Kimpe Andy <andykimpe@gmail.com>
2014-01-01 22:03:29 +0100

    Update 10_1_1.sh

--- install/beta/Ubuntu-12_10/10_1_1.sh
+++ install/beta/Ubuntu-12_10/10_1_1.sh
@@ -42,13 +42,15 @@ source $ZPXISOLANGUAGE.sh
 
 # First we check if the user is 'root' before allowing installation to commence
 if [ $UID -ne 0 ]; then
-echo "$installroot"
+echo "$txt_installroot"
     exit 1
 fi
 
 # Lets check for some common control panels that we know will affect the installation/operating of ZPanel.
 if [ -e /usr/local/cpanel ] || [ -e /usr/local/directadmin ] || [ -e /usr/local/solusvm/www ] || [ -e /usr/local/home/admispconfig ] || [ -e /usr/local/lxlabs/kloxo ] ; then
-echo "$panel"
+    echo -e "$txt_panel1"
+    echo -e "$txt_panel2"
+    echo -e "$txt_panel3"
     exit
 fi
 
@@ -61,11 +63,11 @@ else
 OS=$(uname -s)
   VER=$(uname -r)
 fi
-echo "$osdetect : $OS $VER $BITS"
+echo "$txt_osdetect : $OS $VER $BITS"
 if [ "$OS" = "Ubuntu" ] && [ "$VER" = "12.10" ]; then
 echo "Ok."
 else
-echo "$installsyserror Ubuntu 12.10."
+echo "$txt_installsyserror Ubuntu 12.10."
   exit 1;
 fi
 
@@ -87,7 +89,8 @@ passwordgen() {
 
 # Display the 'welcome' splash/user warning info..
 echo -e '*****************************************************************'
-echo -e "$gpl"
+echo -e "$txt_gpl1"
+echo -e "$txt_glp2"
 echo -e '*****************************************************************'
 
 # Set some installation defaults/auto assignments
