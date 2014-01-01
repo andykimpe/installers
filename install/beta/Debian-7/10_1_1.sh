10ebc02e36a90a97a842585ef97a76f757f8665d
Kimpe Andy <andykimpe@gmail.com>
2014-01-01 20:16:47 +0100

    Update 10_1_1.sh

--- install/beta/Debian-7/10_1_1.sh
+++ install/beta/Debian-7/10_1_1.sh
@@ -231,17 +231,18 @@ ln -s /etc/zpanel/panel/bin/setzadmin /usr/bin/setzadmin
 chmod +x /etc/zpanel/panel/bin/zppy
 chmod +x /etc/zpanel/panel/bin/setso
 cp -R /etc/zpanel/panel/etc/build/config_packs/ubuntu_12_04/. /etc/zpanel/configs/
-sed -i "s|YOUR_ROOT_MYSQL_PASSWORD|$password|" /etc/zpanel/panel/cnf/db.php
+# password configure after check connexion
 cc -o /etc/zpanel/panel/bin/zsudo /etc/zpanel/configs/bin/zsudo.c
 sudo chown root /etc/zpanel/panel/bin/zsudo
 chmod +s /etc/zpanel/panel/bin/zsudo
 
 # MySQL specific installation tasks...
 service mysql start
-mysqladmin -u root password $password
+mysqladmin -u root password $password > /dev/null 2>&1
 until mysql -u root -p$password -e ";" > /dev/null 2>&1 ; do
 read -s -p "$txt_mysqlpassworderror : " password
 done
+sed -i "s|YOUR_ROOT_MYSQL_PASSWORD|$password|" /etc/zpanel/panel/cnf/db.php
 mysql -u root -p$password -e "DROP DATABASE test";
 mysql -u root -p$password -e "DELETE FROM mysql.user WHERE User='root' AND Host != 'localhost'";
 mysql -u root -p$password -e "DELETE FROM mysql.user WHERE User=''";
